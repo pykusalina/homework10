538918aa943b2a4062cf4c5fa88fb8b6b36a8fce
Alina Pykus <alinapykus17@email.com>
2022-08-30 00:08:12 +0300

    created files & add some changes

---
+++ index.js
@@ -0,0 +1,28 @@
+// TAsk 1
+$("h2").add(target="_blank");
+
+// Task 2
+$("#head").css({backgroundColor: "green"});
+$(".inner").css({fontSize: "35px"});
+
+//Task 3
+let div = $("div");
+let h3 = $("h3");
+
+jquery(document).ready(function change() {
+h3 = div;
+})
+change();
+
+// Task 4
+
+let box = $("#checkbox");
+
+jquery(document).ready(function func() {
+   if ( document.forms.box.checked) {
+    return true;
+   } else {
+    return false;
+   }
+
+})
